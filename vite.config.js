e0369c54713e53831dcb6d28d8e9c11bf7a04a00
Lucas <lucasdesouzza728@gmail.com>
2026-06-28 10:05:36 -0300

    perf: otimizacao de chunks e fix calc() no Store.vue

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,19 @@ export default defineConfig({
       output: {
         manualChunks: {
           'vue-vendor': ['vue', 'vue-router', 'pinia'],
+          'views-politicas': [
+            './src/views/PoliticaEntrega.vue',
+            './src/views/PoliticaPrivacidade.vue',
+            './src/views/PoliticaCookies.vue',
+            './src/views/Devolucoes.vue',
+            './src/views/Garantia.vue',
+            './src/views/TermosCustodia.vue',
+          ],
+          'views-heavy': [
+            './src/views/AdminDashboard.vue',
+            './src/views/Configuracoes.vue',
+            './src/views/Sobre.vue',
+          ],
         }
       }
     }
