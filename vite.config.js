36831f7a577fc856ffc2be216e007b337106d369
Lucas <lucasdesouzza728@gmail.com>
2026-06-28 11:10:06 -0300

    fix: corrige alias @ para tecnologia/src

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+    alias: { '@': fileURLToPath(new URL('./tecnologia/src', import.meta.url)) }
   },
   build: {
     chunkSizeWarningLimit: 600,
@@ -14,19 +14,19 @@ export default defineConfig({
         manualChunks: {
           'vue-vendor': ['vue', 'vue-router', 'pinia'],
           'views-politicas': [
-            './src/views/PoliticaEntrega.vue',
-            './src/views/PoliticaPrivacidade.vue',
-            './src/views/PoliticaCookies.vue',
-            './src/views/Devolucoes.vue',
-            './src/views/Garantia.vue',
-            './src/views/TermosCustodia.vue',
+            './tecnologia/src/views/PoliticaEntrega.vue',
+            './tecnologia/src/views/PoliticaPrivacidade.vue',
+            './tecnologia/src/views/PoliticaCookies.vue',
+            './tecnologia/src/views/Devolucoes.vue',
+            './tecnologia/src/views/Garantia.vue',
+            './tecnologia/src/views/TermosCustodia.vue',
           ],
           'views-heavy': [
-            './src/views/AdminDashboard.vue',
-            './src/views/Configuracoes.vue',
-            './src/views/Sobre.vue',
-            './src/views/Gamer.vue',
-            './src/views/Store.vue',
+            './tecnologia/src/views/AdminDashboard.vue',
+            './tecnologia/src/views/Configuracoes.vue',
+            './tecnologia/src/views/Sobre.vue',
+            './tecnologia/src/views/Gamer.vue',
+            './tecnologia/src/views/Store.vue',
           ],
         }
       }
